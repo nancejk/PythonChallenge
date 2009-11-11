ebb6e85a873d05764df3158be9470fbcdfcc3996
Jared Nance <nancejk@u.washington.edu>
2009-11-11 14:42:28 -0800

    Aha...

--- 6.py
+++ 6.py
@@ -1,14 +1,19 @@
 from zipfile import ZipFile
+from re import compile
 
+target = compile('Next nothing is ([0-9]{1,5})')
 zobj = ZipFile('/Users/nancejk/Desktop/channel.zip','r')
-
 basename = 'channel/'
 # It says in the archive to start from 90052.
-first = 90052
+suffix = 90052
 
 while True:
-  temp = zobj.open(basename + str(first) + '.txt')
-  print(temp.read())
-
-
+  temp = zobj.open(basename + str(suffix) + '.txt').read().decode()
+  if target.match(temp):
+   suffix = target.match(temp).groups(0)[0] 
+   print(temp, suffix)
+  else:
+    print(temp)
+    break
+  
 
