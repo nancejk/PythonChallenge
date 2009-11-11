d5984d9605f6d7f7bb144f2742b5813aaa70c51c
Jared Nance <nancejk@u.washington.edu>
2009-11-11 10:52:06 -0800

    Added solution to #3.

--- 3.py
+++ 3.py
@@ -1,8 +1,17 @@
 import re
 from urllib.request import urlopen
 
-target = re.compile('.*^[A-Z][A-Z][A-Z][A-Z]([a-z])[A-Z][A-Z][A-Z]^[A-Z].*')
+# The regex we will use for matching.
+target = re.compile('[^A-Z][A-Z][A-Z][A-Z]([a-z])[A-Z][A-Z][A-Z][^A-Z]')
 
-urldata = urlopen('http://www.pythonchallenge.com/pc/def/equality.html') 
+# Raw data from the page source.
+urldata = urlopen('http://www.pythonchallenge.com/pc/def/equality.html').read().decode() 
 
-print(target.match(urldata.read().decode()))
+# Grab just the data bit.
+subset = urldata[urldata.find('!')+3:urldata.rfind('>')-2]
+
+# Get rid of nasty newline characters introduced by decode.
+subset = subset.replace('\n','')
+
+# Print the answer.
+print(''.join(re.findall(target,subset)))
