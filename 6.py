f42ddd544bec32eb9f3c95ec48a790b2ee26091c
Jared Nance <nancejk@u.washington.edu>
2009-11-11 16:15:08 -0800

    Solved 6.

--- 6.py
+++ 6.py
@@ -1,19 +1,32 @@
 from zipfile import ZipFile
 from re import compile
 
+# We need this old guy again...
 target = compile('Next nothing is ([0-9]{1,5})')
+
+# Had to download this one.  Note that when I unzipped and 
+# rezipped the file, this challenge step didn't work!
 zobj = ZipFile('/Users/nancejk/Desktop/channel.zip','r')
-basename = 'channel/'
+
 # It says in the archive to start from 90052.
 suffix = 90052
 
+# The result will be a string, as usual.
+result = ''
 while True:
-  temp = zobj.open(basename + str(suffix) + '.txt').read().decode()
-  if target.match(temp):
-   suffix = target.match(temp).groups(0)[0] 
-   print(temp, suffix)
+  objname = str(suffix) + '.txt'
+  # Open the temporary file object from the zip archive.
+  tempf = zobj.open(objname).read().decode()
+  # As instructed, add the comment to the string.
+  result += zobj.getinfo(objname).comment.decode()
+  if target.match(tempf):
+    # Look for the next target.
+   suffix = target.match(tempf).groups(0)[0] 
+   # Catch errors... and the ending.
   else:
-    print(temp)
+    print(tempf)
     break
-  
 
+# Print out the result.  Note that you'll have to do just
+# a little more work after you get it...
+print(result)
